6083fc08d0b60375f2187770bd1c71f019b4b501
dmitry.petuhov <dmitry.petuhov@profitclicks.ru>
2021-07-22 13:07:54 +0300

    Async await

--- js/async_await.js
+++ js/async_await.js
@@ -1,41 +1,20 @@
 // Урок 8. JavaScript. Как работает Async, Await. Работа с сервером c fetch
-const delay = (ms) => {
-	return new Promise((resolve) => {
-		setTimeout(() => {
-			const data = {name: 'Dmitriy', age: 27};
-
-			resolve(data);	
-		}, ms);
-	});
-};
-
-// delay(2000).then((data) => console.log(data));
-
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
 function fetchTodos(url) {
-	console.log('Fetch start');
-
-	return delay(2000)
-		.then(() => fetch(url))
-		.then(dataJson => dataJson.json());
+	return fetch(url).then(response => response.json());
 }
 
-fetchTodos(url)
-	.then(data => console.log(data));
+fetchTodos(url).then(data => console.log(data));
 
 
 // Та же функция с помощью async/await
 // async пишется перед function
 // await пишится перед возвратом Promise
 const asyncFetchTodos = async (url) => {
-	console.log('Fetch start');
+	const response = await fetch(url);
 
-	await delay(2000);
-	const dataJson = await fetch(url);
-
-	return await dataJson.json();
+	return await response.json();
 };
 
-asyncFetchTodos(url)
-	.then(data => console.log(data));
+asyncFetchTodos(url).then(data => console.log(data));
