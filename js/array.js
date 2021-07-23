2120bcf34fe0d5e9253107a8faaf9f3ecf92af94
dmitry.petuhov <dmitry.petuhov@profitclicks.ru>
2021-07-23 08:24:15 +0300

    Array methods

--- js/array.js
+++ js/array.js
@@ -1,28 +1,65 @@
 // Урок 12. JavaScript. Методы массивов (forEach, map, filter, reduce, find, findIndex). Js Массивы.
 const arr = [
-	{ name: 'Alex', age: 30, budget: 5200 },
-	{ name: 'Ben', age: 62, budget: 3000 },
-	{ name: 'Drue', age: 22, budget: 2000 },
-	{ name: 'Eddie', age: 39, budget: 1500 },
-	{ name: 'Piter', age: 20, budget: 2500 },
-	{ name: 'Gwen', age: 5, budget: 500 }
+  { name: 'Alex', age: 30, budget: 5200 },
+  { name: 'Ben', age: 62, budget: 3000 },
+  { name: 'Drue', age: 22, budget: 2000 },
+  { name: 'Eddie', age: 39, budget: 1500 },
+  { name: 'Piter', age: 20, budget: 2500 },
+  { name: 'Gwen', age: 5, budget: 500 },
+  { name: 'Alex', age: 99, budget: 5650 },
 ];
+
+// console.log(arr);
 
 // Цикл for
 for (let index = 0; index < arr.length; index++) {
-	console.log(arr[index]);	
+  // console.log(arr[index]);
 }
 
 // Цикл for of
 for (let item of arr) {
-	console.log(item);
+  // console.log(item);
 }
 
 // forEach
-arr.forEach(item => {
-	console.log(item);
+arr.forEach((item, idx, arr) => {
+  // console.log(item);
 });
 
 // map
-const newArr = arr.map(item => item.age + 100);
-console.log(newArr);
+// const mappedArr = arr.map(item => {
+// 	item.age = item.age + 100;
+
+// 	return item;
+// });
+
+// console.log(mappedArr);
+
+// filter
+const filteredArr = arr.filter((item) => item.budget >= 2000);
+// console.log(filteredArr);
+
+// reduce
+const accum = arr.map((item) => item.budget).reduce((prev, current) => prev + current, 0);
+// console.log(accum);
+
+// find
+const found = arr.find((item) => item.name === 'Alex');
+// console.log(found);
+
+// findIndex
+const index = arr.findIndex((item) => item.budget === 500);
+// console.log(index);
+
+// =================
+const amount = arr
+  .filter((item) => item.budget >= 3000)
+  .map((item) => {
+    return {
+      info: `${item.name} (${item.age})`,
+      budget: item.budget,
+    };
+  })
+	.reduce((prev, curr) => prev + curr.budget, 0);
+
+console.log(amount);
